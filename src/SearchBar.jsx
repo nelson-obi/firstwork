06155079c6b10a9abb5e3ebe20f5ec61160bbba2
username <useremail@gmail.com>
2024-12-10 12:42:25 +0100

    New Commit

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -5,8 +5,12 @@ import { IoSearchSharp } from "react-icons/io5";
 export default function SearchBar() {
   return (
         <div className=" border-2 m-4 w-60 h-12 rounded-full px-4 flex justify-between items-center ">
-            <input type="text" name='' placeholder='Search' />
+            <input type="text" name='' placeholder='Search' className='outline-none' />
             <IoSearchSharp className='text-xl' />
         </div>
   )
 }
+
+git add . 
+git commit -m "New Commit"
+git push -u origin main
